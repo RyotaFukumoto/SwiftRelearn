81090b820b81351d293bf63f416fe576fb3066e5
RyotaFukumoto <k014c1106@it-neec.jp>
2025-06-08 11:15:44 +0900

    Initial Commit

---
+++ SwiftRelearn/SwiftRelearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRelearnApp.swift
+//  SwiftRelearn
+//
+//  Created by Ryota F on 2025/06/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRelearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
