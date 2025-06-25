a0fb42088feb2f6d00f89d260fe82c62223a5f0d
RyotaFukumoto <k014c1106@it-neec.jp>
2025-06-25 23:29:20 +0900

    CatInfoModifierのスタイル設定にコメントを追加

--- SwiftRelearn/Modifiers/CatInfoModifier.swift
+++ SwiftRelearn/Modifiers/CatInfoModifier.swift
@@ -4,11 +4,11 @@ import SwiftUI
 struct CatInfoModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .font(.headline)
-            .foregroundColor(.orange)
-            .padding(8)
-            .background(Color(.systemGray6))
-            .cornerRadius(10)
+            .font(.headline)    // フォントを太字の見出し（headline）に設定
+            .foregroundColor(.orange)   // 文字色をオレンジ色に設定
+            .padding(8) // 8ポイントのパディングを追加
+            .background(Color(.systemGray6))    // 背景色をシステムグレー6に設定
+            .cornerRadius(10)   // 角を10ポイントの半径で丸める
     }
 }
 
